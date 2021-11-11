bea5d490f990a0a7367dd35b6183d84e51b2415c
ferpalma21 <ferpalma21@gmail.com>
2021-11-11 21:28:32 +0100

    adding more examples

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import scanner #librería a utilizar para scannear puertos
 
-# Ejemplo de función
-ports = scanner.getOpenPorts("http://localhost:8080")
-print("Open ports:", ports)
+# Ejemplo de función básica
+ports = scanner.getOpenPorts("tusitio.com")
+print("Open ports basic:", ports)
+# Determinando Puertos
+ports = scanner.getOpenPorts("unam.mx", [79, 82])
+print("Open ports range given:", ports)
+# Verbosidad
+ports = scanner.getOpenPorts("hotmail.con", [], True)
+print("Open ports range give + verbose:", ports)
