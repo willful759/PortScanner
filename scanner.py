d16166f024f87d532bd15d9525a29ff114311bdc
willful759 <haloreachgrados@hotmail.com>
2021-11-11 14:29:19 -0600

    first scanner implementation

--- scanner.py
+++ scanner.py
@@ -3,16 +3,19 @@
 import socket #library to use
 from ports import ports_and_services
 
-def getOpenPorts(target, portRange)
+def getOpenPorts(target, portRange):
     openPorts = []
     #Obtener el host con socket
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+        #hacer un loop sobre el rango de puertos
+        for port in portRange:
+            #abrir el socket determinado
+            #verificar si se conecta
+            try:
+                sock.connect((target,port))
+                #si se conecta agregarlo al array
+                openPorts.append(port)
+            except ConnectionRefusedError:
+                pass
 
-    #hacer un loop sobre el rango de puertos
-    
-    #abrir el socket determinado
-
-    #verificar si se conecta
-
-    #si se conecta agregarlo al array
     return openPorts
-
