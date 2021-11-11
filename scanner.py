6d2bdd1b0e09e09c963c2cb699c7ca5509ea87aa
willful759 <haloreachgrados@hotmail.com>
2021-11-11 15:18:30 -0600

    fix for positional argument portRange

--- scanner.py
+++ scanner.py
@@ -3,19 +3,20 @@
 import socket #library to use
 from ports import ports_and_services
 
-def getOpenPorts(target, portRange):
+def getOpenPorts(target, portRange=None):
     openPorts = []
-    #Obtener el host con socket
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
-        #hacer un loop sobre el rango de puertos
-        for port in portRange:
-            #abrir el socket determinado
-            #verificar si se conecta
-            try:
-                sock.connect((target,port))
-                #si se conecta agregarlo al array
-                openPorts.append(port)
-            except ConnectionRefusedError:
-                pass
+    if portRange is None:
+        portRange = ports_and_services.keys()
+    #hacer un loop sobre el rango de puertos
+    for port in portRange:
+        #verificar si se conecta
+        try:
+            #Obtener el host con socket
+            s = socket.create_connection((target,port))
+            #si se conecta agregarlo al array
+            openPorts.append(port)
+            s.close()
+        except ConnectionRefusedError:
+            pass
 
     return openPorts
