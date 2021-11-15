f0316d763b2c0be0c2b5883da9de56cc37e7e1d8
willful759 <haloreachgrados@hotmail.com>
2021-11-14 19:44:47 -0600

    fixed various issues

--- scanner.py
+++ scanner.py
@@ -2,21 +2,43 @@
 
 import socket #library to use https://docs.python.org/3/library/socket.html
 from ports import ports_and_services
+from urllib.parse import urlparse
 
-def getOpenPorts(target, portRange=None):
-    openPorts = []
-    if portRange is None:
+def validate(url):
+    try:
+        urlparse(url)
+        return True
+    except:
+        return False
+
+def getOpenPorts(target, portRange=None,verbose=False):
+    if not validate(target):
+        return None
+
+    if portRange is None or len(portRange) == 0:
         portRange = ports_and_services.keys()
-    #hacer un loop sobre el rango de puertos
-    for port in portRange:
-        #verificar si se conecta
-        try:
-            #Obtener el host con socket
-            s = socket.create_connection((target,port))
-            #si se conecta agregarlo al array
-            openPorts.append(port)
-            s.close()
-        except ConnectionRefusedError:
-            pass
+    else:
+        portRange = range(portRange[0],portRange[1] + 1)
+    
+    openPorts = []
+    try:
+        ips = socket.gethostbyname_ex(target)[2]
+        host = ips[0]
+        """
+        if set(ips) > 1:
+            print(f"scanning {target} ({host}),\nother addresses: {ips}")
+        """
+        for port in portRange:
+            sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            sock.settimeout(0.5)
+            if not sock.connect_ex((host,port)):
+                if verbose:
+                    openPorts.append((port,ports_and_services.get(port)))
+                else:
+                    openPorts.append(port)
+            sock.close()
+    except socket.gaierror:
+        pass
+        #print(f"failed to resolve: {target}")
 
     return openPorts
